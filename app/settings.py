22b915d214a9c86c6634cf9c6563aec67e071851
Petr Vey <68317575+PetrVey@users.noreply.github.com>
2022-05-30 09:52:12 +0800

    Add files via upload
    Updated secret key

--- app/settings.py
+++ app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h+40!vrs+lq(za@v)b-x0b-o*$7(i++efh@biz^#-)*)1=1z1s'
+SECRET_KEY = get_env_variable('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
